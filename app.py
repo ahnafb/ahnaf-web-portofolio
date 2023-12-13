3ebe3dda17d3a9b5b8eff0243ef2092d34511745
ahnafb <ahnafb@students.unnes.ac.id.com>
2023-12-13 23:12:25 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["UPLOAD_FOLDER"] = "./static/profile_pics"
+
+SECRET_KEY = "AHNAF"
+
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("index.html")
+    
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
