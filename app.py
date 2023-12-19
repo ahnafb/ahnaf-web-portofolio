dee260cea947de824d2e44b9f2d9e462b93f0f6a
ahnafb <ahnafb@students.unnes.ac.id.com>
2023-12-19 15:40:37 +0700

    rapihin loader efek + tambah efek open web opacity

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ SECRET_KEY = "AHNAF"
 @app.route("/", methods=['GET'])
 def home():
     return render_template("index.html")
-    
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
