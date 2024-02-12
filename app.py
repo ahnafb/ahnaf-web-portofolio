575ebdfe37e26afd0da52708d905894ee9df5ac9
ahnafb <ahnafb@students.unnes.ac.id.com>
2024-02-12 18:49:25 +0700

    add more projects

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ SECRET_KEY = "AHNAF"
 def home():
     return render_template("index.html")
 
+@app.route("/project", methods=['GET'])
+def project():
+    return render_template("project.html")
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
